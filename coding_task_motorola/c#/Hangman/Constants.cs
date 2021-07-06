2f4580c79dcde8e27d4edbbebe91ff9db22d8b30
Sylwia Leung <sylwiokot@Sylwias-MBP.DWR-921E>
2021-07-06 22:04:00 +0200

    Add static content

---
+++ coding_task_motorola/c#/Hangman/Constants.cs
@@ -0,0 +1,11 @@
+﻿using Hangman.CsvManagers;
+using Hangman.Models;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    public static class Constants
+    {
+        public static List<CountryCityPair> EuropeanCapitals = InputManager.ReadCountryCapitalPairs();
+    }
+}
