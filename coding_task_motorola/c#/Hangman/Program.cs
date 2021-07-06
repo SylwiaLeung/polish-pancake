b0101e72ee73c0202ee0d681c5a5ee42cef9a7f1
Sylwia Leung <sylwiokot@Sylwias-MBP.DWR-921E>
2021-07-06 22:06:46 +0200

    Add program entrypoint

---
+++ coding_task_motorola/c#/Hangman/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Hangman.CsvManagers;
+using Hangman.Engine;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman game");
+            var gameEngine = new GameEngine();
+            var resultsManager = new ResultsFileManager();
+            while (true)
+            {
+                var score = gameEngine.RunGame();
+                if (score != null)
+                {
+                    resultsManager.ManageHighScores(score);
+                    resultsManager.PrintResults();
+                }
+
+                Console.WriteLine("Game over!");
+                Console.WriteLine("Would you like to quit?");
+                Console.WriteLine("Press [y] to quit or press Enter to continue the fun!");
+                var response = Console.ReadLine();
+                if (response.ToLower() == "y")
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Thank you for playing Hangman!");
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+
+    }
+}
