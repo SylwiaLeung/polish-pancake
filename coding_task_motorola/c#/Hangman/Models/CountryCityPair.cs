c466c9a66964f451b3763b8b2873ce5bcc0da058
Sylwia Leung <sylwiokot@Sylwias-MBP.DWR-921E>
2021-07-06 22:04:18 +0200

    Models

---
+++ coding_task_motorola/c#/Hangman/Models/CountryCityPair.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman.Models
+{
+    public class CountryCityPair
+    {
+        public string Country { get; }
+        public string City { get; }
+
+        public CountryCityPair(string country, string city)
+        {
+            Country = country;
+            City = city;
+        }
+        public sealed class CountryCityPairMap : ClassMap<CountryCityPair>
+        {
+            public CountryCityPairMap()
+            {
+                Map(m => m.Country).Index(0);
+                Map(m => m.City).Index(1);
+            }
+        }
+    }
+}
---
+++ coding_task_motorola/c#/Hangman/Models/HighScoreRecord.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman.Models
+{
+    public class HighScoreRecord
+    {
+
+        public DateTime Date { get; }
+        public string Name { get; }
+        public string City { get; }
+        public int Tries { get; }
+        public double GuessingTime { get; }
+
+        public HighScoreRecord(DateTime date, string name, string city, int tries, double guessingTime)
+        {
+            Date = date;
+            Name = name;
+            City = city;
+            Tries = tries;
+            GuessingTime = guessingTime;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} - Time: {GuessingTime} - number of tries: {Tries} - date: {Date} - guessed word: {City}";
+        }
+
+        public sealed class HighScoreRecordMap : ClassMap<HighScoreRecord>
+        {
+            public HighScoreRecordMap()
+            {
+                Map(m => m.Date).Index(0);
+                Map(m => m.Name).Index(1);
+                Map(m => m.City).Index(2);
+                Map(m => m.Tries).Index(3);
+                Map(m => m.GuessingTime).Index(4);
+            }
+        }
+    }
+}
